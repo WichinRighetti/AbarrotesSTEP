ac8e1cf1a5e6281adc680ebd9fdd31ffb8b05c32
WichinRighetti <lamrighetti@gmail.com>
2023-10-05 19:11:53 -0700

    updated subCat y cat
    models y controllers

---
+++ controllers/categoriaController.php
@@ -0,0 +1,36 @@
+<?php
+    //Alow acces from outside the server
+    header('Access-Control-Allow-origin: *');
+    //Allow methods
+    header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE'); //Read, inster, update, delete
+
+
+    require_once($_SERVER['DOCUMENT_ROOT'].'/AbarrotesSTEP/models/categoria.php');
+
+
+    //GET reaad
+    if($_SERVER['REQUEST_METHOD'] == "GET"){
+        //Parameter
+        if(isset($_GET['categoria_id'])){
+            try{
+                $c = new Categoria($_GET['categoria_id']);
+                //Display
+                echo json_encode(array(
+                    'status' => 0,
+                    'state' => json_decode($c->toJson())
+                ));
+            }catch(RecordNotFoundException $ex){
+                echo json_encode(array(
+                    'status' => 1,
+                    'errorMessage' => $ex->get_message()
+                ));
+            }
+        }else{
+            //Display
+            echo json_encode(array(
+                'status' => 0,
+                'state' => json_decode(Categoria::getAllByJson())
+            ));
+        }
+    }
+?>
