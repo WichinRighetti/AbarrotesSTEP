9af1a2748d630b0bc14a86848151c4a1039addcc
Martin-Vg <MartinnVg@gmail.com>
2023-10-05 19:39:52 -0700

    Added entrada controller and model

---
+++ controllers/entradaController.php
@@ -0,0 +1,33 @@
+<?php
+    //ALLOW ACCESS FROM OUTSIDE THE SERVER
+    header('Access-Control-Allow-Origin: *');
+    //Allow methods
+    header('Access-Control-Methods: GET, POST, PUT, DELETE');
+
+    require_once($_SERVER['DOCUMENT_ROOT'].'/abarrotesStep/models/entrada.php');
+
+    //get (read)
+    if($_SERVER['REQUEST_METHOD'] == 'GET'){
+        //parameters
+        if(isset($_GET['entrada_id'])){
+            try{
+                $a = new Almacen($_GET['entrada_id']);
+                //display
+                echo json_encode(array(
+                    'status'=> 0,
+                    'entrada' => json_decode(($a->toJson()))
+                ));
+            }catch(RecordNotFOundException $ex){
+                echo json_encode(array(
+                    'status' => 1,
+                    'errorMessage' => $ex->get_message()
+                ));
+            }
+        }else{
+            echo json_encode(array(
+                'status' => 0,
+                'entrada' => json_decode(entrada::getAllByJson())
+            ));
+        }
+    }
+?>
