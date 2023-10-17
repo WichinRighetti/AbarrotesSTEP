534cb6d9b12fb15d5130717944664dcc36fbd704
WichinRighetti <lamrighetti@gmail.com>
2023-10-16 18:22:11 -0700

    updated DB query

---
+++ DB/spSalida.sql
@@ -0,0 +1,11 @@
+use sistemainventario;
+
+delimiter $$
+CREATE PROCEDURE `spEntrada`(IN `registroCantidad ` INT, IN `registroId` INT) 
+BEGIN -- Insertar la cantidad en la tabla entrada
+INSERT INTO entrada (entrada.inventario_id ,entrada.cantidad) VALUES (registroId, registroCantidad);
+-- Actualizar la cantidad en la tabla inventario 
+UPDATE inventario SET inventario.cantidad = inventario.cantidad - registroCantidad 
+WHERE inventario.inventario_id = registroId; 
+END$$
+delimiter
