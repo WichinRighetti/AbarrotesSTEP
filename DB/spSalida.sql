3ddbd7397d29dbd471560a2bb0b56181e0f50dbb
WichinRighetti <lamrighetti@gmail.com>
2023-10-16 19:54:18 -0700

    update SPs

--- DB/spSalida.sql
+++ DB/spSalida.sql
@@ -1,11 +1,9 @@
-use sistemainventario;
-
-delimiter $$
-CREATE PROCEDURE `spEntrada`(IN `registroCantidad ` INT, IN `registroId` INT) 
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `spSalida`(registroCantidad INT, registroId INT)
 BEGIN -- Insertar la cantidad en la tabla entrada
-INSERT INTO entrada (entrada.inventario_id ,entrada.cantidad) VALUES (registroId, registroCantidad);
+INSERT INTO salida (salida.inventario_id , salida.cantidad) VALUES (registroId, registroCantidad);
 -- Actualizar la cantidad en la tabla inventario 
-UPDATE inventario SET inventario.cantidad = inventario.cantidad - registroCantidad 
+UPDATE inventario SET inventario.cantidad = (inventario.cantidad - registroCantidad) 
 WHERE inventario.inventario_id = registroId; 
 END$$
-delimiter
+DELIMITER ;
